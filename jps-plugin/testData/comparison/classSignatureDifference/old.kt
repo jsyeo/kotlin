a6ce2d44ab6a1d6f1b3e074848a3be2c80317d7a
Michael Nedzelsky <Michael.Nedzelsky@jetbrains.com>
2015-08-05 16:38:19 +0300

    tests for comparison of changes in classes

---
+++ jps-plugin/testData/comparison/classSignatureDifference/old.kt
@@ -0,0 +1,16 @@
+package test
+
+class ClassWithFlagsChanged {
+    public fun main() {}
+}
+
+class ClassWithTypeParameterListChanged<U, V> {
+    public fun main() {}
+}
+
+class ClassWithSuperTypeListChanged {
+    public fun main() {}
+}
+
+deprecated("") class ClassWithClassAnnotationListChanged
+
---
+++ jps-plugin/testData/comparison/privateOnlyDifference/old.kt
@@ -0,0 +1,83 @@
+package test
+
+class ClassWithPrivateFunAdded {
+    public fun main() {}
+    val s = java.lang.String.valueOf(20)
+}
+
+class ClassWithPrivateFunRemoved {
+    public fun main() {}
+    private fun privateFun() {}
+}
+
+class ClassWithPrivateFunSignatureChanged {
+    public fun main() {}
+    private fun privateFun(arg: String) {}
+}
+
+class ClassWithPrivateValAdded {
+    public fun main() {}
+}
+
+class ClassWithPrivateValRemoved {
+    public fun main() {}
+    private val x: Int = 100
+}
+
+class ClassWithPrivateValSignatureChanged {
+    public fun main() {}
+    private val x: Int = 100
+}
+
+class ClassWithPrivateVarAdded {
+    public fun main() {}
+}
+
+class ClassWithPrivateVarRemoved {
+    public fun main() {}
+    private var x: Int = 100
+}
+
+class ClassWithPrivateVarSignatureChanged {
+    public fun main() {}
+    private var x: Int = 100
+}
+
+class ClassWithGetterForPrivateValChanged {
+    public fun main() {}
+    private val x: Int = 100
+}
+
+class ClassWithGetterAndSetterForPrivateVarChanged {
+    public fun main() {}
+    private var x: Int = 100
+}
+
+class ClassWithPrivatePrimaryConstructorAdded {
+    public fun main() {}
+    private constructor(arg: Int) {}
+}
+
+class ClassWithPrivatePrimaryConstructorRemoved private constructor() {
+    public fun main() {}
+    private constructor(arg: Int) : this() {}
+}
+
+class ClassWithPrivatePrimaryConstructorChanged private constructor() {
+    public fun main() {}
+}
+
+class ClassWithPrivateSecondaryConstructorsAdded {
+    public fun main() {}
+}
+
+class ClassWithPrivateSecondaryConstructorsRemoved() {
+    private constructor(arg: Int): this() {}
+    private constructor(arg: String): this() {}
+    public fun main() {}
+}
+
+
+
+
+
